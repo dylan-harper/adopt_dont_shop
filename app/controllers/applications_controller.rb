89af1cf3624689846e8a3e3e0589669c8b3a08c1
Dylan Harper <dylanharper4@icloud.com>
2021-12-12 10:40:50 -0800

    Finish most functionality for starting an app

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,4 +4,32 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    application = Application.create(name: application_params[:name],
+                                     description: application_params[:description],
+                                     status: application_params[:status])
+    #ask how to encapsulate the below better
+    address = application.create_address(street_address: application_params[:street_address],
+                             city: application_params[:city],
+                             state: application_params[:state],
+                             zip_code: application_params[:zip_code])
+
+    redirect_to "/applications/#{application.id}"
+  end
+
+  private
+
+  def application_params
+    params.permit(:name,
+                  :street_address,
+                  :city,
+                  :state,
+                  :zip_code,
+                  :description,
+                  :status)
+  end
+
 end
---
+++ spec/features/applications/create_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'create application' do
+  describe 'new application' do
+    it 'renders the new form' do
+      visit '/applications/new'
+
+      expect(page).to have_content('New Application')
+      expect(find('form')).to have_content('Name')
+      expect(find('form')).to have_content('Street address')
+      expect(find('form')).to have_content('City')
+      expect(find('form')).to have_content('State')
+      expect(find('form')).to have_content('Zip code')
+      expect(find('form')).to have_content('Description')
+    end
+  end
+
+  describe 'create application' do
+    context 'given valid data' do
+      it 'creates a new application' do
+        visit '/applications/new'
+
+        fill_in 'Name', with: 'Joe J'
+        fill_in 'Street address', with: '100 Longhorn Lane'
+        fill_in 'City', with: 'Houston'
+        fill_in 'State', with: 'Texas'
+        fill_in 'Zip code', with: 12345
+        fill_in 'Description', with: "Big yard and loving family"
+
+        click_button('Save')
+
+        expect(page).to have_current_path(current_url)#works, but would like to make it more dynamic
+        expect(page).to have_content('Joe J')
+    end
+
+    end
+  end
+end
